0e91a03ccf82091519fadab2eb69247764a5b0b4
Jones <jones0716@gmail.com>
2017-11-02 16:48:07 +0800

    第一支composer 做的網頁

---
+++ index.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<H1>Hello World~</H1>
+<H1>哈囉~</H1>
+<?php
+
+include_once __DIR__."/vendor/autoload.php";
+
+ use Carbon\Carbon;
+
+ printf("Right Now is %s", Carbon::now()->toDateTimeString());
+
+?>
+
+</body>
+</html>
